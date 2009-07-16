67b97b8b85cf2bf8d82e7ae1a9431ef6f6d79f6b
Ron DuPlain <rduplain@nrao.edu>
2009-07-16 09:35:33 -0400

    Posted documents in CSV to Solr instance.

--- manage/post.py
+++ manage/post.py
@@ -4,9 +4,12 @@ from pysolr import Solr
 
 from docs import docs
 
-solr_url = 'http://avalon.cv.nrao.edu:8983/solr'
+print len(docs)
+
+solr_url = 'http://localhost:8983/solr'
 conn = Solr(solr_url)
 
 conn.delete(q='*:*')
 conn.commit()
 conn.add(docs)
+conn.commit()
