9247eabbab90210a11a1868c03eee66ca3225ec5
Ron DuPlain <rduplain@nrao.edu>
2009-09-10 11:11:17 -0400

    Adding title_t field, for blacklight use.

--- manage/docs.py
+++ manage/docs.py
@@ -10,5 +10,8 @@ for raw_doc in raw_docs:
         v = v or ''
         doc[k.lower().strip().replace(' ', '_')] = v.decode('latin1')
         doc['id'] = doc.get('primary_key', '')
+        doc['title_t'] = doc.get('author', '')
+        if doc.get('institution', ''):
+            doc['title_t'] += ' -- %s' % doc['institution']
     if doc.get('id', ''):
         docs.append(doc)
