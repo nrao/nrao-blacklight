5eb63b93075d6d2b6911b0ae2d6a613f8dc83dc6
Ron DuPlain <rduplain@nrao.edu>
2009-09-15 18:05:53 -0400

    Now heavily massaging fields, creating Solr docs.

--- manage/docs.py
+++ manage/docs.py
@@ -1,19 +1,49 @@
-# See example.data.csv.
-
 import csv
+import operator
 
 raw_docs = [record for record in csv.DictReader(open('data.csv'))]
 docs = []
 
-# This can be simplified, but let's make it work first.
+field_map = {'Instruments': ('12 Meter','36 Foot','85 Foot','140 Foot',
+                             '300 Foot','Archival VLA','GBI','GBT',
+                             'Technical','VLA','VLBA','VLBI','VSOP',
+                             'Non-Observational','MK II Correlator',),
+             'Surveys': ('CGPS','VGPS','MAGPIS','NVSS','SPGS','FIRST',),
+             'NRAO Libraries': ('Charlottesville','Socorro','Green Bank',),
+             }
+
+mapped_fields = reduce(operator.add, field_map.values())
+mapped_fields += ('Other', 'Other (Write in)')
+
+# Heavily massage fields for creating dicts (pysolr) for Solr docs.
 for raw_doc in raw_docs:
-    doc = dict()
-    for k,v in raw_doc.items():
+    doc = {'Title': '',
+           'Author': '',
+           'Institution': '',
+           'Year': '',
+           'Instruments': [],
+           'Surveys': [],
+           'Electronic Location': '',
+           'ADS Bibcode': '',
+           'NRAO Libraries': [],
+           'Notes': '',
+           'PreDoc': '',
+           }
+    for k,v in [(k,v) for k,v in raw_doc.items() if k not in mapped_fields]:
         v = v or ''
-        doc[k.lower().strip().replace(' ', '_')] = v.decode('latin1')
-        doc['id'] = doc.get('primary_key', '')
-        doc['title_t'] = doc.get('author', '')
-        if doc.get('institution', ''):
-            doc['title_t'] += ' -- %s' % doc['institution']
-    if doc.get('id', ''):
-        docs.append(doc)
+        v = v.decode('latin1')
+        doc[k] = v
+
+    for key in field_map.keys():
+        ks = field_map[key]
+        doc[key] = [k for k,v in raw_doc.items() if k in ks and str(v) == '1']
+
+    other = raw_doc.get('Other (Write in)', '')
+    if other:
+        doc['Surveys'].append(other)
+
+    doc['id'] = doc.pop('Primary Key', '')
+    doc['title_t'] = '%s / %s' % (doc['Title'], doc['Author'])
+    doc['Electronic Location'] = doc['Electronic Location'].strip('#')
+
+    docs.append(doc)
