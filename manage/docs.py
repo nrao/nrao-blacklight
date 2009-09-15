6859974d15fb38d21a12a6d3ee35e5d17a6baecd
Ron DuPlain <rduplain@nrao.edu>
2009-09-15 16:16:09 -0400

    Working docs.py. Note backslash escape char.

--- manage/docs.py
+++ manage/docs.py
@@ -1,3 +1,16 @@
 # See example.docs.py.
 
+import csv
+
+raw_docs = [record for record in csv.DictReader(open('data.csv'), escapechar='\\')]
 docs = []
+
+# This can be simplified, but let's make it work first.
+for raw_doc in raw_docs:
+    doc = dict()
+    for k,v in raw_doc.items():
+        v = v or ''
+        doc[k.lower().strip().replace(' ', '_')] = v.decode('latin1')
+        doc['id'] = doc.get('filename', '')
+    if doc.get('id', ''):
+        docs.append(doc)
