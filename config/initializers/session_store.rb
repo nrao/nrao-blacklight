ad4c29489ff29c39a323e137d76d5f24e57759f7
Ron DuPlain <rduplain@nrao.edu>
2009-11-30 15:19:52 -0500

    Customized to 'telescope-data'.
    Find files to change with `grep -ilR nrao .*`.
    For good measure, ran `rake db:sessions:create`, followed by
    `rm db/development.sqlite3` then `rake db:migrate`
    This repository contains some security information; change before deployment.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,11 +2,11 @@
 
 # Your secret key for verifying cookie session data integrity.
 # If you change this key, all old sessions will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
+# Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 ActionController::Base.session = {
-  :key         => '_nrao-blacklight_session',
-  :secret      => 'a60c3ec07058bae80b43f4977eb336c152306799ca45ca05b3a973d8a56c2c66ed41c77d1a57988a7cb30702c8740a29b6b334909f98542b6a91893e044061fc'
+  :key         => '_telescope-data_session',
+  :secret      => 'b8437d7cde318d64736e860eea604a4ad76f745d102bd621407d4442d789f740c2fff09667d4b9e6c54285623dabf24031a6dd0e6b7e6334e821cf5a7b72566c'
 }
 
 # Use the database for sessions instead of the cookie-based default,
