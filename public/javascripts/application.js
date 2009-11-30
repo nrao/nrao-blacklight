a0af743ceb1b874f44344d4b850a0aa2f359b8a2
Ron DuPlain <rduplain@nrao.edu>
2009-11-30 14:53:25 -0500

    Copied files from plugin for customization.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,9 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+$(function(){
+	var bl = new Blacklight();
+});
+
+$(document).ready(function() {
+  // adds classes for zebra striping table rows
+  $('table.zebra tr:even').addClass('zebra_stripe');
+  $('ul.zebra li:even').addClass('zebra_stripe');
+});
