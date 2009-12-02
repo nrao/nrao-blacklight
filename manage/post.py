3e4741bc0f190fc7d86cc7518136352989adaeda
Ron DuPlain <rduplain@nrao.edu>
2009-12-01 21:12:02 -0500

    Ready to migrate commits from theses-manage.

--- manage/post.py
+++ manage/post.py
@@ -1,25 +1,12 @@
 #!/usr/bin/env python
 
-import sys
-
 from pysolr import Solr
 
 from docs import docs
 
-print 'number of docs:', len(docs)
-
-solr_url = 'http://localhost:8990/solr'
-print 'connecting to Solr at', solr_url
-
+solr_url = 'http://avalon.cv.nrao.edu:8983/solr'
 conn = Solr(solr_url)
 
-if '-d' in sys.argv:
-    print 'deleting docs in Solr'
-    conn.delete(q='*:*')
-    conn.commit()
-else:
-    print "use flag '-d' to delete all Solr docs before adding docs"
-
-print 'adding docs to Solr'
-conn.add(docs)
+conn.delete(q='*:*')
 conn.commit()
+conn.add(docs)
