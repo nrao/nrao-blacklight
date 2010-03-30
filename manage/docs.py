81c0cd57cfc0c17c9a3cda8f053ca4a4b2bd01da
Ron DuPlain <rduplain@nrao.edu>
2010-03-30 15:10:32 -0400

    Added Band field.

--- manage/docs.py
+++ manage/docs.py
@@ -34,6 +34,56 @@ def skyfreq(record):
     else:
         return float(given) / 1000000000.0
 
+def price_is_right(bands, freq):
+    "Return the closest band in bands without going over target."
+    freq_band = None
+    if freq is None or not bands:
+        return None
+    for band_key in sorted(bands.keys()):
+        if band_key > freq:
+            break
+        freq_band = bands[band_key]
+    return freq_band
+
+bands = {0.0: '4-band',
+         0.225: 'P-band',
+         0.390: '450-band',
+         0.510: '600-band',
+         0.680: '800-band',
+         1.0: 'L-band',
+         2.0: 'S-band',
+         4.0: 'C-band',
+         8.0: 'X-band',
+         12.0: 'Ku-band',
+         18.0: 'K-band',
+         26.5: 'Ka-band',
+         40.0: 'Q-band',
+         50.0: 'V-band',
+         75.0: 'W-band',
+         }
+
+def band(record):
+    """Create a band parameter given a record with skyfreq.
+
+    From Dana, 2010-03-30:
+    4-band:   near 74 MHz
+    P-band:   0.225 -   0.390 GHz
+    450-band: 0.390 -   0.510 GHz
+    600-band: 0.510 -   0.680 GHz
+    800-band: 0.680 -   1.0   GHz
+    L-band:   1.0   -   2.0   GHz
+    S-band:   2.0   -   4.0   GHz
+    C-band    4.0   -   8.0   GHz
+    X-band:   8.0   -  12.0   GHz
+    Ku-band: 12.0   -  18.0   GHz (sometimes called U)
+    K-band:  18.0   -  26.5   GHz
+    Ka-band: 26.5   -  40.0   GHz
+    Q-band:  40.0   -  50.0   GHz
+    V-band:  50.0   -  75.0   GHz (typically not used due to O2 line)
+    W-band:  75.0   - 110.0   GHz 
+    """
+    return price_is_right(bands, skyfreq(record))
+
 def doc_it(record):
     if record.get('PROJID') and record.get('SCAN'):
         doc_id = record.get('PROJID') + '-' + record.get('SCAN')
@@ -50,7 +100,7 @@ def doc_it(record):
              ('velocity', velocity(record)),
              ('veldef', record.get('VELDEF')),
              ('skyfreq', skyfreq(record)),
-             ('', record.get('')),
+             ('band', band(record)),
              ('', record.get('')),
              ('', record.get('')),
              ('', record.get('')),
