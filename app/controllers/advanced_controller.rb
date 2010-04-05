31b63163e52e4ab731f8e5ccb192eb4ea8ff6a7f
Ron DuPlain <rduplain@nrao.edu>
2010-04-05 00:56:00 -0600

    Demonstrating redirects in Advanced search.

--- app/controllers/advanced_controller.rb
+++ app/controllers/advanced_controller.rb
@@ -1,2 +1,9 @@
 class AdvancedController < ApplicationController
+  def index
+    redirect_to :action => 'gbt'
+  end
+
+  def gbt
+    # redirect_to :controller => 'catalog', :q => 'i:balser'
+  end
 end
