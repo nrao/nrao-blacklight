8c177d76b9df76414a38ec2aea833bcb5834bbc7
Ron DuPlain <rduplain@nrao.edu>
2010-04-05 02:15:37 -0600

    Initial cut of a working advanced search.

--- app/controllers/advanced_controller.rb
+++ app/controllers/advanced_controller.rb
@@ -4,6 +4,19 @@ class AdvancedController < ApplicationController
   end
 
   def gbt
-    # redirect_to :controller => 'catalog', :q => 'i:balser'
+  end
+
+  def submit
+    q = ""
+    keys = [:project, :i, :observer, :object, :band, :obstype, 
+            :receiver, :backend, :procname]
+    keys.each do |key|
+      value = params[key]
+      if value and not value.blank?
+        q += " " + key.to_s + ":" + value.to_s
+      end
+    end
+    q.strip!
+    redirect_to :controller => 'catalog', :q => q
   end
 end
