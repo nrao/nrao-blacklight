6277a3f090f911435a2db36ee3d70d5c6bbb78f3
Ron DuPlain <rduplain@nrao.edu>
2010-09-21 10:54:25 -0400

    Avoiding duplicate doc ids.

--- manage/docs.py
+++ manage/docs.py
@@ -5,10 +5,17 @@ import user
 strptime = datetime.datetime.strptime
 
 def _docs(fd, **kwargs):
+    ids = set()
     for doc in csv.DictReader(fd):
         doc['start'] = strptime(doc['start'], '%y-%b-%d %H:%M:%S').strftime('%Y-%m-%dT%H:%M:%SZ')
         doc['stop'] = strptime(doc['stop'], '%y-%b-%d %H:%M:%S').strftime('%Y-%m-%dT%H:%M:%SZ')
-        doc['id'] = doc['project'] + '_' + doc['start']
+        candidate_id = doc['project'] + '_' + doc['start']
+        doc['id'] = candidate_id
+        count = 1
+        while doc['id'] in ids:
+            count += 1
+            doc['id'] = candidate_id + '-%d' % count
+        ids.add(doc['id'])
         doc['telescope'], doc['configuration'], doc['sub'], doc['nant'] =\
             doc['tele_conf_sub_nant'].split(':')
         yield doc
