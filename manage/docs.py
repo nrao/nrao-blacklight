17ab0b6914caeaeb626ab2ac30542f14ab102d5a
Ron DuPlain <rduplain@nrao.edu>
2010-09-23 10:30:02 -0400

    Working demo against VLA obs summary.

--- manage/docs.py
+++ manage/docs.py
@@ -7,14 +7,14 @@ strptime = datetime.datetime.strptime
 def _docs(fd, **kwargs):
     ids = set()
     for doc in csv.DictReader(fd):
+        candidate_id = doc['project'] + strptime(doc['start'], '%y-%b-%d %H:%M:%S').strftime('_%Y-%m-%d_%H:%M:%S')
         doc['start'] = strptime(doc['start'], '%y-%b-%d %H:%M:%S').strftime('%Y-%m-%dT%H:%M:%SZ')
         doc['stop'] = strptime(doc['stop'], '%y-%b-%d %H:%M:%S').strftime('%Y-%m-%dT%H:%M:%SZ')
-        candidate_id = doc['project'] + '_' + doc['start']
         doc['id'] = candidate_id
         count = 1
         while doc['id'] in ids:
             count += 1
-            doc['id'] = candidate_id + '-%d' % count
+            doc['id'] = candidate_id + '%s' % chr(96 + count) # 97 = ascii 'a'
         ids.add(doc['id'])
         doc['telescope'], doc['configuration'], doc['sub'], doc['nant'] =\
             doc['tele_conf_sub_nant'].split(':')
---
+++ manage/post.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import sys
+
+from pysolr import Solr
+
+from docs import docs
+
+solr_url = 'http://perseus.cv.nrao.edu:8996/solr'
+print 'connecting to Solr at', solr_url
+
+conn = Solr(solr_url)
+
+if '-d' in sys.argv:
+    print 'deleting docs in Solr'
+    conn.delete(q='*:*')
+    conn.commit()
+else:
+    print "use flag '-d' to delete all Solr docs before adding docs"
+
+print 'adding docs to Solr (interrupt as needed)'
+count = 0
+try:
+    for doc in docs:
+        conn.add([doc])
+        count += 1
+except KeyboardInterrupt:
+    print
+    print count, 'docs'
+conn.commit()
